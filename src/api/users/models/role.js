e6487d123cc01bccd382fec1919b9c59aaa9c2b8
Raul Rizzo Ribeiro <raulrr88@gmail.com>
2018-06-18 19:41:39 -0300

    Models Update

--- src/api/users/models/role.js
+++ src/api/users/models/role.js
@@ -1,26 +1,28 @@
-const Sequelize = require('sequelize')
+const { sequelize, Sequelize } = require('../../../database/superhero-database')
 
 const schema = {
-    id: {
-      type: Sequelize.BIGINT,
-      primaryKey: true,
-      autoIncrement: true,
-      unique: true
-    },
-    name: {
-      type: Sequelize.String,
-      allowNull: false,
-      unique: true
-    },
-    created_at: Sequelize.DATE,
-    updated_at: Sequelize.DATE
-  }
+  id: {
+    type: Sequelize.BIGINT,
+    primaryKey: true,
+    autoIncrement: true,
+    unique: true
+  },
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    unique: true
+  },
+  created_at: Sequelize.DATE,
+  updated_at: Sequelize.DATE
+}
 
-  const options = {
-    timestamps: true,
-    paranoid: false,
-    underscored: true,
-    freezeTableName: true
-  }
-  
-  module.exports = sequelize.define('Role', schema, options);
+const options = {
+  timestamps: true,
+  paranoid: false,
+  underscored: true,
+  freezeTableName: true
+}
+
+const RoleModel = sequelize.define('RoleModel', schema, options);
+
+module.exports = RoleModel;
--- src/api/users/models/user.js
+++ src/api/users/models/user.js
@@ -1,30 +1,38 @@
-const Sequelize = require('sequelize')
+const { sequelize, Sequelize } = require('../../../database/superhero-database')
+const bcrypt = require('bcryptjs');
 
 const schema = {
-    id: {
-      type: Sequelize.BIGINT,
-      primaryKey: true,
-      autoIncrement: true,
-      unique: true
-    },
-    username: {
-      type: Sequelize.String,
-      allowNull: false,
-      unique: true
-    },
-    password: {
-      type: Sequelize.STRING(128),
-      allowNull: false
-    },
-    created_at: Sequelize.DATE,
-    updated_at: Sequelize.DATE
-  }
+  id: {
+    type: Sequelize.BIGINT,
+    primaryKey: true,
+    autoIncrement: true,
+    unique: true
+  },
+  username: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    unique: true
+  },
+  password: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  created_at: Sequelize.DATE,
+  updated_at: Sequelize.DATE
+}
 
-  const options = {
-    timestamps: true,
-    paranoid: false,
-    underscored: true,
-    freezeTableName: true
-  }
-  
-  module.exports = sequelize.define('User', schema, options);
+const options = {
+  timestamps: true,
+  paranoid: false,
+  underscored: true,
+  freezeTableName: true
+}
+
+sequelize.addHook('beforeCreate', async (schema) => {
+  const hash = await bcrypt.hash(schema.password, 10);
+  schema.password = hash;
+});
+
+const UserModel = sequelize.define('UsersModel', schema, options);
+
+module.exports = UserModel;
