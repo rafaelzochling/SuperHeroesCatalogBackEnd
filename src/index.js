d15145d57c812951ada92db82fa00e7e057ac4fa
Raul Rizzo Ribeiro <raulrr88@gmail.com>
2018-06-20 07:06:56 -0300

    Cors Dependency Added

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+var cors = require('cors')
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+// app.get('/products/:id', function (req, res, next) {
+//     res.json({ msg: 'This is CORS-enabled for all origins!' })
+// });
 
 require('./api/users/route-manager')(app);
 
