b5964e1535353fcbff09c01ba23464ef25442871
Raul Rizzo Ribeiro <raulrr88@gmail.com>
2018-06-20 07:28:36 -0300

    Comment Remove

--- src/index.js
+++ src/index.js
@@ -7,10 +7,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
-// app.get('/products/:id', function (req, res, next) {
-//     res.json({ msg: 'This is CORS-enabled for all origins!' })
-// });
-
 require('./api/users/route-manager')(app);
 
 app.listen(3000);
