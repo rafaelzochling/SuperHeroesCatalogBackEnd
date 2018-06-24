f55442cbadbb6e46022b5b42d62e5f78e2ecb725
Raul Rizzo Ribeiro <raulrr88@gmail.com>
2018-06-23 23:48:23 -0300

    Audit Events Model

---
+++ src/api/models/event.js
@@ -0,0 +1,35 @@
+const { sequelize, Sequelize } = require('../../database/superhero-database')
+
+const schema = {
+  id: {
+    type: Sequelize.BIGINT,
+    primaryKey: true,
+    autoIncrement: true,
+    unique: true
+  },
+  entity: {
+    type: Sequelize.STRING,
+  },
+  entityid: {
+    type: Sequelize.STRING,
+  },
+  username: {
+    type: Sequelize.STRING,
+  },
+  action: {
+    type: Sequelize.STRING,
+  },
+  created_at: Sequelize.DATE,
+  updated_at: Sequelize.DATE
+}
+
+const options = {
+  timestamps: true,
+  paranoid: false,
+  underscored: true,
+  freezeTableName: true
+}
+
+const EventModel = sequelize.define('eventstable', schema, options);
+
+module.exports = EventModel;
